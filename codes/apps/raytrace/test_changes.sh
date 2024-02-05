9d3853f239ede2312f125ea2146d89095b05ef3f
sskean2 <sskean2@uic.edu>
2024-02-04 23:20:19 -0600

    Switched to using -a3.

--- codes/apps/raytrace/test_changes.sh
+++ codes/apps/raytrace/test_changes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make RAYTRACE && (rm inputs/teapot.rl
-time ./RAYTRACE -a1 inputs/teapot.env
-md5sum -c inputs/teapot_a1.md5sum)
+time ./RAYTRACE -a3 inputs/teapot.env
+md5sum -c inputs/teapot_a3.md5sum)
 rm *.c *.o *.h
