a7157687e74c187d2958bd3b26cb24146a28165f
sskean2 <sskean2@uic.edu>
2024-02-04 21:54:24 -0600

    Removed the obvious mmap call that does basically nothing, and removed the truly evil macro.

--- codes/apps/raytrace/main.C
+++ codes/apps/raytrace/main.C
@@ -256,7 +256,6 @@ int	main(int argc, CHAR *argv[])
             case 'a':
             case 'A':
                 AntiAlias = TRUE;
-								mmap(0,1<<32,PROT_WRITE,MAP_SHARED|MAP_POPULATE|MAP_ANONYMOUS,0,0);
                 if (argv[i][2] != '\0')
                 {   NumSubRays = atoi(&argv[i][2]); }
                 else
@@ -319,7 +318,6 @@ int	main(int argc, CHAR *argv[])
 
     MaxGlobMem <<= 20;			/* Convert MB to bytes.      */
     MAIN_INITENV(,MaxGlobMem + 512*1024)
-		gb = sizeof(GMEM);
     gm = (GMEM *)G_MALLOC(sizeof(GMEM));
 
 
