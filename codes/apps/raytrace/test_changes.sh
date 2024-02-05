a5b73e826ce206ddb72f5c3cd21e2f15965d50c2
sskean2 <sskean2@uic.edu>
2024-02-05 01:05:09 -0600

    'Switched' to -a1000 just for the heck of it. It looks like my program has a consistent lead over RAYTRACE_orig, and always produces the same output for the teapot.env input file, which is the only one that I know works. Seems underwhelming though, and in the current state --switch-events didn't help me at all. Maybe Jacob used it to check for page faults? I'll go back and test it there, see if it helps; and ask him if he knew about the --event=faults ability. This was a pretty cool assignment if I've finished, I was definitely well-equipped - I just needed to remember to use all my tools. gdb's backtrace really saved my bacon for discovering where the __printf thing was being called.

--- codes/apps/raytrace/test_changes.sh
+++ codes/apps/raytrace/test_changes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 make RAYTRACE && (rm inputs/teapot.rl
-time ./RAYTRACE -a100 inputs/teapot.env
-md5sum -c inputs/teapot_a100.md5sum)
+time ./RAYTRACE -a1000 inputs/teapot.env
+md5sum -c inputs/teapot_a1000.md5sum)
 rm *.c *.o *.h
